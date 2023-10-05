248532ccd145661ee975aa9a0a89d4949b37e6c6
WyattDalton <59888887+WyattDalton@users.noreply.github.com>
2023-10-04 20:25:54 -0700

    testing makign authed requests to wp 2.0

--- src/app/_actions.tsx
+++ src/app/_actions.tsx
@@ -2,6 +2,7 @@
 
 import { getUserData } from '@/components/utils/getUserData';
 import { MongoClient } from 'mongodb';
+import { cookies } from 'next/headers';
 
 // ###
 // ### Get mongo client
@@ -21,3 +22,54 @@ export const getMongoDB = async () => {
     const db = client.db(_mongoUserId);
     return db;
 };
+
+/**
+ * Retrieves the authenticated user data from the Maker Digital API.
+ * If the environment is not production, it uses the dev auth.
+ * @returns {Promise<any>} A promise that resolves to the user data if successful, or false if unsuccessful.
+ */
+export const getAuth = async () => {
+
+    // check if userData has already been set, and if it has, returned the cached data
+
+    let authData: any;
+    const cookieStore = cookies();
+
+    // if the env is not production, use the dev auth
+    if (process.env.NODE_ENV !== 'production') {
+        authData = {
+            'id': process.env.DEV_AUTH_ID,
+            'auth': process.env.DEV_AUTH_TOKEN,
+        }
+    } else {
+        authData = cookieStore.get('mkr_user') as any;
+    }
+
+    const id = authData?.id;
+    const auth = authData?.auth;
+
+    const reqUrl = `https://makerdigital.io/wp-json/makerdigital/v1/get-user-data/${id}`;
+
+    try {
+
+        const authorizedUser = await fetch(reqUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${auth}`,
+            },
+        })
+
+        // If the response is not 200, return false
+        if (authorizedUser.status !== 200) {
+            return false;
+        }
+
+        // If the response is 200, return the user data
+        const userData = await authorizedUser.json();
+        return userData;
+    } catch (err) {
+        console.log('ERROR: ', err);
+        return false;
+    }
+
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import AppNavigation from '@/components/AppNav'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { cookies } from 'next/headers'
+import { getAuth } from './_actions'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,32 +17,21 @@ export default function RootLayout({
   children: React.ReactNode
   }) {
 
-  const cookieStore = cookies();
-  const nonce = cookieStore.get('mkr_user');
-
-  const auth = fetch('https://makerdigital.io/wp-json/wp/v2/makerdigital/v1/get-current-user', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-WP-Nonce': `${nonce}`,
-    },
-  }).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log('auth: ', data)
-  })
+  const auth = getAuth().then((res) => { return true; }).catch((err) => { return false; });
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex min-h-screen bg-gray-100 p-4 gap-4">
-          <div className="flex flex-col w-1/4 max-w-[300px]">
-            <AppNavigation />
+        {!!auth ?
+          <div className="flex min-h-screen bg-gray-100 p-4 gap-4">
+            <div className="flex flex-col w-1/4 max-w-[300px]">
+              <AppNavigation />
+            </div>
+            <div className="flex flex-col min-h-screen grow-1 w-full">
+              {children}
+            </div>
           </div>
-          <div className="flex flex-col min-h-screen grow-1 w-full">
-            {children}
-          </div>
-        </div>
+          : 'Loading...'}
       </body>
     </html>
   )

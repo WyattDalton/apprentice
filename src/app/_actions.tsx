f3088e047df7cfbaf13cf9106247862f377b4706
WyattDalton <59888887+WyattDalton@users.noreply.github.com>
2023-10-04 20:38:49 -0700

    testing making authed requests to wp 2.5 Removes mongo ref from _actions

--- src/app/_actions.tsx
+++ src/app/_actions.tsx
@@ -1,27 +1,6 @@
 'use server';
 
-import { getUserData } from '@/components/utils/getUserData';
-import { MongoClient } from 'mongodb';
 import { cookies } from 'next/headers';
-
-// ###
-// ### Get mongo client
-export const getMongoDB = async () => {
-    const client = new MongoClient(process.env.MONGODB_URL as string);
-
-    // Get User Data
-    const rawUserData = await getUserData();
-    const userId = rawUserData.userId.toString().toLowerCase().replace(/ /g, '_');
-    const userOrganization = rawUserData.organization
-        .toLowerCase()
-        .replace(/ /g, '_');
-    const _mongoUserId = `${userId}-${userOrganization}`;
-
-    // Get mongodb client
-    await client.connect();
-    const db = client.db(_mongoUserId);
-    return db;
-};
 
 export const getAuth = async () => {
 

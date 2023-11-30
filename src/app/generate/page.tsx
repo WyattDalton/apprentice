2340a4cbe4be58ec498508e949af0bac1495ea8b
WyattDalton <59888887+WyattDalton@users.noreply.github.com>
2023-11-30 14:43:59 -0800

    Adds an auth check before accessing mongo

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,4 +1,5 @@
 
+import { getAuth } from "../_actions";
 import { getDataFromAPI } from "./_actions";
 import Generator from "./_components/Generator";
 import GeneratorDashboard from "./_components/GeneratorDashboard";
@@ -6,6 +7,8 @@ export const runtime = 'edge'
 
 export default async function Page() {
 
+    // When fired from here, the mongo db does not connect. It seems t be firing before a call to get the user auth is sent. So I need to find a way to cache the user auth and then use it here.
+    const authData = await getAuth();
     const data = await getDataFromAPI(false);
 
     return <Generator threadsData={data?.threads || []} tonesData={data?.tones || []} formulasData={data?.formulas || []} sources={data?.sources || []} metaData={data?.meta || []} />

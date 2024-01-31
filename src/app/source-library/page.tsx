810e15ef490a66ae39000187f4c7ef9aa7b0b185
WyattDalton <59888887+WyattDalton@users.noreply.github.com>
2024-01-31 10:01:14 -0800

    troubleshooting build errors 1.02

--- src/app/source-library/page.tsx
+++ src/app/source-library/page.tsx
@@ -1,5 +1,5 @@
 'use server';
-import { fetchHtmlFromUrl, processHtmlFromUrl, addUrl, fetchSources, deleteSource } from './_actions';
+import { fetchHtmlFromUrl, processHtmlFromUrl, addUrl, addRaw, addFiles, fetchSources, deleteSource } from './_actions';
 import SourcesUi from './_components/SourcesUi';
 import { getMongoDB } from '@/utils/getMongo';
 
@@ -12,9 +12,11 @@ export default async function Page() {
         <SourcesUi
             sources={sources}
             fetchHtmlFromUrl={fetchHtmlFromUrl}
-            processHtmlFromUrl={processHtmlFromUrl}
-            addUrl={addUrl}
             deleteSource={deleteSource}
+            addFile={addFiles}
+            addRaw={addRaw}
+            addUrl={addUrl}
+            processHtmlFromUrl={processHtmlFromUrl}
         />
     )
 }

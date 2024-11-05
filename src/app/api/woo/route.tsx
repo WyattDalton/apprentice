394242a7560a25b5a7f8fa3f016405b12721c7f7
WyattDalton <59888887+WyattDalton@users.noreply.github.com>
2024-11-05 13:01:33 -0800

    Updates prisma schema and implementation

--- src/app/api/woo/route.tsx
+++ src/app/api/woo/route.tsx
@@ -1,5 +1,39 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getMongoDB } from '@/utils/getMongo';
+import prisma from '@/utils/getPrisma';
+
+const createMongoUser = async (userId: string, username: string) => {
+    //### Init db
+    let db;
+
+    //### Get db for {userId}-{organization}
+    db = await getMongoDB(userId, username) as any;
+
+    //### Access the users collection
+    const collection = db.collection("users");
+
+    // ### Upsert the user to the collection with information from request body
+    const res = await collection.updateOne({ userId: userId }, { $set: { userId: userId, username: username } }, { upsert: true });
+    return res;
+}
+
+const createPrismaUser = async (userId: string, username: string) => {
+
+    // Convert data to strings
+    if (typeof userId !== 'string' || typeof username !== 'string') {
+        userId = userId.toString();
+        username = username.toString();
+    }
+
+    //### Create or update user in Prisma
+    const user = await prisma.user.upsert({
+        where: { id: userId },
+        update: { username: username },
+        create: { id: userId, username: username },
+    });
+
+    return user;
+}
 
 /**
  * POST request handler for updating user data in MongoDB.
@@ -14,22 +48,13 @@ export async function POST(req: NextRequest) {
         const body = await req.json();
         const { userId, username } = body;
 
-        //### Init db
-        let db;
-
-        //### Get db for {userId}-{organization}
-        db = await getMongoDB(userId, username) as any;
-
-        //### Access the users collection
-        const collection = db.collection("users");
-
-        // ### Upsert the user to the collection with information from request body
-        const res = await collection.updateOne({ userId: userId }, { $set: { userId: userId, username: username } }, { upsert: true });
+        // const user = await createMongoUser(userId, username);
+        const user = await createPrismaUser(userId, username);
 
         // ### Return a JSON response with a success flag and updated user data
         return NextResponse.json({
             'success': true,
-            'user': res,
+            'user': user,
         });
 
 
